bada99f0129637ff218653ecce912c06b7056841
vrezhMik <mikayelyan.vrezh99@gmail.com>
2025-01-14 22:54:03 +0400

    initial commit

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps(['name'])
+</script>
+
+<template>
+  <button>{{ props.name }}</button>
+</template>
+
+<style scoped lang="scss">
+@import './../styles/variables';
+
+button {
+  padding: 1rem 1.2rem;
+  font-size: 0.9rem;
+  border: 1px solid $primary-grey;
+  border-radius: 50px;
+  color: $primary-black;
+  transition: 0.2s ease;
+  &:hover {
+    background-color: $primary-orange;
+    color: $primary-white;
+    transition: 0.2s ease;
+  }
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Button from './ButtonComponent.vue'
+</script>
+
+<template>
+  <header :class="['container', 'flex', 'sora']">
+    <nav :class="['header-menu', 'flex']">
+      <a href="/"><span>D</span>ooin</a>
+      <a href="/">home</a>
+      <a href="#product">product</a>
+      <a href="#about">about</a>
+    </nav>
+    <div :class="['']">
+      <Button name="Get Started"></Button>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@import './../styles/variables';
+header {
+  border-bottom: 1px solid $primary-grey;
+  height: 50px;
+  padding: 30px 0;
+  align-items: center;
+  position: sticky;
+  justify-content: space-between;
+  top: 0;
+  .header-menu {
+    width: 30%;
+    column-gap: 30px;
+    a {
+      font-size: 14px;
+      color: $primary-black;
+      transition: 0.2s ease;
+      font-weight: 400;
+      &:first-child {
+        font-size: 16px;
+        font-weight: 600;
+      }
+      &:hover,
+      span {
+        transition: 0.2s ease;
+        color: $primary-blue;
+      }
+    }
+  }
+}
+</style>
